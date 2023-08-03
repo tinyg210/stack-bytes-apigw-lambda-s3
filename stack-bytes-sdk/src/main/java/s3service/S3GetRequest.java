e573afbe6b8b7a9f27799f047a61ae8627e119e8
AncaGhenade <ancaghenade@gmail.com>
2023-08-03 22:56:47 +0200

    add sdk module + some refactor

---
+++ stack-bytes-sdk/src/main/java/s3service/S3GetRequest.java
@@ -0,0 +1,59 @@
+package s3service;
+
+import static s3service.S3Configs.BUCKET_NAME;
+import static s3service.S3Configs.s3Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+public class S3GetRequest {
+
+
+  public static void main(String[] args) {
+
+    String objectKey = "Shrek.txt";
+
+    try {
+      // get the object from the S3 bucket
+      GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+          .bucket(BUCKET_NAME)
+          .key(objectKey)
+          .build();
+
+      ResponseInputStream<GetObjectResponse> response = s3Client.getObject(getObjectRequest);
+
+      // read the text content from the file
+      String objectText = readResponseInputStream(response);
+
+      System.out.println("Object text: " + objectText);
+
+    } catch (S3Exception | IOException e) {
+      System.err.println("Error reading object from S3: " + e.getMessage());
+    }
+
+    s3Client.close();
+  }
+
+  private static String readResponseInputStream(
+      ResponseInputStream<GetObjectResponse> responseInputStream)
+      throws IOException {
+    try (InputStream inputStream = responseInputStream;
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+      StringBuilder stringBuilder = new StringBuilder();
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        stringBuilder.append(line + "\n");
+      }
+      return stringBuilder.toString();
+    }
+  }
+
+}
---
+++ stack-bytes-sdk/src/main/java/s3service/S3PostRequest.java
@@ -0,0 +1,40 @@
+package s3service;
+
+import static s3service.S3Configs.BUCKET_NAME;
+import static s3service.S3Configs.s3Client;
+
+import java.nio.file.Paths;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+public class S3PostRequest {
+
+
+  public static void main(String[] args) {
+
+    // local file to upload
+    String filePath = "stack-bytes-sdk/src/main/resources/Fiona.txt";
+    String objectKey = "Fiona.txt";
+
+    try {
+      // put the object into the S3 bucket
+      PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+          .bucket(BUCKET_NAME)
+          .key(objectKey)
+          .build();
+
+      PutObjectResponse response = s3Client.putObject(putObjectRequest, Paths.get(filePath));
+
+      if (response != null) {
+        System.out.println("Object uploaded successfully!");
+      } else {
+        System.out.println("Object upload failed!");
+      }
+    } catch (S3Exception s3Exception) {
+      System.out.println("An error occurred: " + s3Exception.getMessage());
+    }
+
+    s3Client.close();
+  }
+}
