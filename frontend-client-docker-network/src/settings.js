e51de857e054ebeecf4c583a91faf37de1c350e2
AncaGhenade <ancaghenade@gmail.com>
2023-08-28 02:27:10 +0200

    clean up

--- frontend-client-docker-network/src/settings.js
+++
@@ -1,5 +0,0 @@
-const settings = {
-  apiGW: '/restapis/id12345/dev/_user_request_/quoteApi',
-};
-
-export default settings;
--- frontend-client-localsurf/src/endpoints.js
+++
@@ -1,5 +0,0 @@
-const endpoints = {
-  apiGW: 'http://id12345.execute-api.amazonaws.com/dev/quoteApi',
-};
-
-export default endpoints;
